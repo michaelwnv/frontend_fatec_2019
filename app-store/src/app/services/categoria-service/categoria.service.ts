b037576b1648ead8d41099e55cd02e000622c694
minicurso <minicurso@LAB-186>
2019-11-20 22:16:53 -0300

    Aula 2

--- app-store/src/app/services/categoria-service/categoria.service.ts
+++ app-store/src/app/services/categoria-service/categoria.service.ts
@@ -16,6 +16,6 @@ export class CategoriaService extends BaseService {
   }
 
   public Listar(): Observable<Categoria[]> {
-      return null;
+    return this.HttpClient.get<Categoria[]>(this.EndPoint("Categoria"))
   }
 }
